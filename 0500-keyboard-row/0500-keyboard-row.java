1bd005d262866f549eb4d0b15b0b1d7283689854
Alexsha <alexshaponchales2@gmail.com>
2025-06-04 22:02:38 +0530

    Time: 1 ms (38.65%), Space: 42 MB (6.53%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,6 +1,6 @@
 class Solution {
-    public String[] findWords(String[] words) {
-        int n=words.length,k=0;
+    public String[] findWords(String[] word) {
+        int n=word.length,k=0;
         String ans[]=new String[n];
         Set<Character> row1 = new HashSet<>();
         Set<Character> row2 = new HashSet<>();
@@ -10,55 +10,22 @@ class Solution {
         for (char c : "zxcvbnmZXCVBNM".toCharArray()) row3.add(c);
         for(int i=0;i<n;i++)
         {
-            int len=words[i].length();
-            int flag=1;
-            for(int j=0;j<len;j++)
+            if(check(row1,word[i])||check(row2,word[i])||check(row3,word[i]))
             {
-                if(!row1.contains(words[i].charAt(j)))
-                {
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag==1)
-            {
-                ans[k++]=words[i];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            int len=words[i].length();
-            int flag=1;
-            for(int j=0;j<len;j++)
-            {
-                if(!row2.contains(words[i].charAt(j)))
-                {
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag==1)
-            {
-                ans[k++]=words[i];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            int len=words[i].length();
-            int flag=1;
-            for(int j=0;j<len;j++)
-            {
-                if(!row3.contains(words[i].charAt(j)))
-                {
-                    flag=0;
-                    break;
-                }
-            }
-            if(flag==1)
-            {
-                ans[k++]=words[i];
+                ans[k++]=word[i];
             }
         }
         return Arrays.copyOf(ans,k);
+    }
+    public static boolean check(Set<Character> row ,String word)
+    {
+        for(char c:word.toCharArray())
+        {
+            if(!row.contains(c))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
