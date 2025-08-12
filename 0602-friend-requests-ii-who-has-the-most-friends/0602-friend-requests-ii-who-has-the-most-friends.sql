1a639880fd52c2d459c19e3a05a3534942c4ea05
Alexsha <alexshaponchales2@gmail.com>
2025-08-12 14:58:22 +0530

    Time: 269 ms (97.98%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,15 +1,4 @@
-SELECT id,
-      (SUM(count))AS num
-FROM (
-    SELECT accepter_id AS id, COUNT(*) AS count
-    FROM RequestAccepted
-    GROUP BY accepter_id
-
-    UNION ALL
-
-    SELECT requester_id AS id, COUNT(*) AS count
-    FROM RequestAccepted
-    GROUP BY requester_id
-) AS num
-GROUP BY id
-order by num desc limit 1;
+select id ,sum(count) as num from (select requester_id as id,count(*) as count from RequestAccepted group by requester_id  
+union all 
+select accepter_id as id ,count(*) as count from RequestAccepted group by accepter_id)as num
+group by id order by num desc limit 1 offset 0;
